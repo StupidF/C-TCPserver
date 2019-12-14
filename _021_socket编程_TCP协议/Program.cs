a2ef2962d10eb3c0955d68d8fbdf7a1588e12fed
stupid.zcy@gmail.com <stupid.zcy@gmail.com>
2019-12-14 22:40:39 +0800

    C#学习之tcp通信服务器

---
+++ _021_socket编程_TCP协议/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021_socket编程_TCP协议
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1、创建socket
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            //2、绑定IP与端口号
+            endpor
+            tcpServer.Bind();
+        }
+    }
+}
